7b26778b35025b0a508f4c4e106bbb49615ff8d2
eyuuo <eyuuo@172.17.144.28>
2019-05-06 17:06:05 +0900

    data model

---
+++ src/main/java/edu/handong/analysis/datamodel/Course.java
@@ -0,0 +1,16 @@
+package edu.handong.analysis.datamodel;
+
+public class Course {
+
+	private String courseName;
+	
+	public Course(String name) {
+		courseName = name;
+	}
+	
+	public String getCourseName() {
+		// TODO Auto-generated method stub
+		return courseName;
+	}
+
+}
---
+++ src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -0,0 +1,16 @@
+package edu.handong.analysis.datamodel;
+
+public class Student {
+	
+	private String name;
+	
+	public Student(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+
+}
